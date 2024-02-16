bdc8744a3c3bc14eefa7f0ae260e299f119cca19
Shuaiqing Luo <luoshuaiqing>
2024-02-15 22:17:15 -0800

    update

--- RWRC/Channels/ChannelsViewController.swift
+++ RWRC/Channels/ChannelsViewController.swift
@@ -41,41 +41,41 @@ final class ChannelsViewController: UITableViewController {
     label.font = UIFont.systemFont(ofSize: 15)
     return label
   }()
-
+  
   private let channelCellIdentifier = "channelCell"
   private var currentChannelAlertController: UIAlertController?
-
+  
   private let database = Firestore.firestore()
   private var channelReference: CollectionReference {
     return database.collection("channels")
   }
-
+  
   private var channels: [Channel] = []
   private var channelListener: ListenerRegistration?
-
+  
   private let currentUser: User
-
+  
   deinit {
     channelListener?.remove()
   }
-
+  
   init(currentUser: User) {
     self.currentUser = currentUser
     super.init(style: .grouped)
-
+    
     title = "Channels"
   }
-
+  
   required init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-
+    
     clearsSelectionOnViewWillAppear = true
     tableView.register(UITableViewCell.self, forCellReuseIdentifier: channelCellIdentifier)
-
+    
     toolbarItems = [
       UIBarButtonItem(title: "Sign Out", style: .plain, target: self, action: #selector(signOut)),
       UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil),
@@ -84,29 +84,29 @@ final class ChannelsViewController: UITableViewController {
       UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonPressed))
     ]
     toolbarLabel.text = AppSettings.displayName
-
+    
     channelListener = channelReference.addSnapshotListener { querySnapshot, error in
       guard let snapshot = querySnapshot else {
         print("Error listening for channel updates: \(error?.localizedDescription ?? "No error")")
         return
       }
-
+      
       snapshot.documentChanges.forEach { change in
         self.handleDocumentChange(change)
       }
     }
   }
-
+  
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
     navigationController?.isToolbarHidden = false
   }
-
+  
   override func viewWillDisappear(_ animated: Bool) {
     super.viewWillDisappear(animated)
     navigationController?.isToolbarHidden = true
   }
-
+  
   // MARK: - Actions
   @objc private func signOut() {
     let alertController = UIAlertController(
@@ -115,21 +115,21 @@ final class ChannelsViewController: UITableViewController {
       preferredStyle: .alert)
     let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
     alertController.addAction(cancelAction)
-
+    
     let signOutAction = UIAlertAction(
       title: "Sign Out",
       style: .destructive) { _ in
-      do {
-        try Auth.auth().signOut()
-      } catch {
-        print("Error signing out: \(error.localizedDescription)")
+        do {
+          try Auth.auth().signOut()
+        } catch {
+          print("Error signing out: \(error.localizedDescription)")
+        }
       }
-    }
     alertController.addAction(signOutAction)
-
+    
     present(alertController, animated: true)
   }
-
+  
   @objc private func addButtonPressed() {
     let alertController = UIAlertController(title: "Create a new Channel", message: nil, preferredStyle: .alert)
     alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
@@ -142,29 +142,29 @@ final class ChannelsViewController: UITableViewController {
       field.returnKeyType = .done
       field.tintColor = .primary
     }
-
+    
     let createAction = UIAlertAction(
       title: "Create",
       style: .default) { _ in
-      self.createChannel()
-    }
+        self.createChannel()
+      }
     createAction.isEnabled = false
     alertController.addAction(createAction)
     alertController.preferredAction = createAction
-
+    
     present(alertController, animated: true) {
       alertController.textFields?.first?.becomeFirstResponder()
     }
     currentChannelAlertController = alertController
   }
-
+  
   @objc private func textFieldDidChange(_ field: UITextField) {
     guard let alertController = currentChannelAlertController else {
       return
     }
     alertController.preferredAction?.isEnabled = field.hasText
   }
-
+  
   // MARK: - Helpers
   private func createChannel() {
     guard
@@ -173,7 +173,7 @@ final class ChannelsViewController: UITableViewController {
     else {
       return
     }
-
+    
     let channel = Channel(name: channelName)
     channelReference.addDocument(data: channel.representation) { error in
       if let error = error {
@@ -181,44 +181,44 @@ final class ChannelsViewController: UITableViewController {
       }
     }
   }
-
+  
   private func addChannelToTable(_ channel: Channel) {
     if channels.contains(channel) {
       return
     }
-
+    
     channels.append(channel)
     channels.sort()
-
+    
     guard let index = channels.firstIndex(of: channel) else {
       return
     }
     tableView.insertRows(at: [IndexPath(row: index, section: 0)], with: .automatic)
   }
-
+  
   private func updateChannelInTable(_ channel: Channel) {
     guard let index = channels.firstIndex(of: channel) else {
       return
     }
-
+    
     channels[index] = channel
     tableView.reloadRows(at: [IndexPath(row: index, section: 0)], with: .automatic)
   }
-
+  
   private func removeChannelFromTable(_ channel: Channel) {
     guard let index = channels.firstIndex(of: channel) else {
       return
     }
-
+    
     channels.remove(at: index)
     tableView.deleteRows(at: [IndexPath(row: index, section: 0)], with: .automatic)
   }
-
+  
   private func handleDocumentChange(_ change: DocumentChange) {
     guard let channel = Channel(document: change.document) else {
       return
     }
-
+    
     switch change.type {
     case .added:
       addChannelToTable(channel)
@@ -235,22 +235,22 @@ extension ChannelsViewController {
   override func numberOfSections(in tableView: UITableView) -> Int {
     return 1
   }
-
+  
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return channels.count
   }
-
+  
   override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
     return 55
   }
-
+  
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: channelCellIdentifier, for: indexPath)
     cell.accessoryType = .disclosureIndicator
     cell.textLabel?.text = channels[indexPath.row].name
     return cell
   }
-
+  
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     let channel = channels[indexPath.row]
     let viewController = ChatViewController(user: currentUser, channel: channel)
